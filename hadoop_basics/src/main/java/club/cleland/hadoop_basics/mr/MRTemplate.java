5e89a46741fd42b9c712b5d0ccfc0fe827e01594
cleland <clelandgt@163.com>
2019-03-25 18:36:57 +0800

    udpate

--- hadoop_basics/src/main/java/club/cleland/hadoop_basics/mr/MRTemplate.java
+++ hadoop_basics/src/main/java/club/cleland/hadoop_basics/mr/MRTemplate.java
@@ -17,137 +17,119 @@ import org.apache.hadoop.util.ToolRunner;
 
 import java.io.IOException;
 
-public class MRTemplate extends Configured implements Tool{
 
+public class MRTemplate extends Configured implements Tool {
     /**
-     * map
-     * TODO
+     * Mapper
      */
-    public static class TemplateMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    public static class TemplateMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            //TODO
+
+        }
 
         @Override
-        public void setup(Context context) throws IOException, InterruptedException {
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
             //TODO
         }
 
         @Override
-        public  void map(LongWritable key, Text value, Context context)
-                throws IOException, InterruptedException {
+        protected void cleanup(Context context) throws IOException, InterruptedException {
             //TODO
         }
-
-        @Override
-        public void cleanup(Context context) throws IOException, InterruptedException {
-            //TODO
-        }
-
     }
 
-
-    /**
-     * reduce
-     * TODO
-     */
-    public static class TemplateReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+    // Reduce
+    public static class TemplateReduce extends Reducer<Text, IntWritable, Text, IntWritable>{
         @Override
-        public void setup(Context context) throws IOException, InterruptedException {
+        protected void setup(Context context) throws IOException, InterruptedException {
             //TODO
         }
 
         @Override
-        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             //TODO
         }
 
         @Override
-        public void cleanup(Context context) throws IOException, InterruptedException {
+        protected void cleanup(Context context) throws IOException, InterruptedException {
             //TODO
         }
     }
 
 
     /**
-     * run
+     *
      * @param args
      * @return
      * @throws Exception
      */
-    public int run(String args[]) throws Exception {
+    @Override
+    public int run(String[] args) throws Exception {
+        // get config
+        Configuration configuration = new Configuration();
 
-        //driver
-        //1) get conf
-        Configuration configuration = this.getConf();
-
-        //2) create job
-        Job job = Job.getInstance(configuration, this.getClass().getSimpleName());
+        // create job
+        Job job = Job.getInstance(configuration, "template");
         job.setJarByClass(this.getClass());
 
-        //3.1)  input
-        Path path = new Path(args[0]);
-        FileInputFormat.addInputPath(job, path);
+        // input
+        Path inputPath = new Path(args[0]);
+        FileInputFormat.addInputPath(job, inputPath);
 
-
-        //3.2) map
-
+        // map
         job.setMapperClass(TemplateMapper.class);
-        //TODO
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(IntWritable.class);
 
-        //1.分区
+        // 1. partition分区
         //job.setPartitionerClass();
 
-        //2.排序
+        // 2. 排序
         //job.setSortComparatorClass();
 
-        //3.combiner
-        //job.setCombinerClass(WordCountCombiner.class);
+        // 3. combiner
+        //job.setCombinerClass();
 
-        //4.compress
+        // 4. compress
         //configuration.set("mapreduce.map.output.compress","true");
         //configuration.set("mapreduce.map.output.compress.codec","org.apache.hadoop.io.compress.SnappyCodec");
 
-        //5.分组
+        // 5. group分组
         //job.setGroupingComparatorClass();
 
-
-        //3.3) reduce
+        // reduce
         job.setReducerClass(TemplateReduce.class);
-        //TODO
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
-        job.setNumReduceTasks(2);
+        // output
+        Path outPath = new Path(args[1]);
+        FileOutputFormat.setOutputPath(job, outPath);
 
-        //3.4) output
-        Path output = new Path(args[1]);
-        FileOutputFormat.setOutputPath(job, output);
-
-        //4) commit
+        // commit
         boolean isSuc = job.waitForCompletion(true);
         return (isSuc) ? 0 : 1;
     }
 
-    public static void main(String[] args) {
-        args = new String[]{
-                "hdfs://loc-header:9000/data/input/wordcount.txt",
-                "hdfs://loc-header:9000/data/output/mr_template"
-        };
-
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args){
         Configuration configuration = new Configuration();
-        try {
-
-            //先判断
+        try{
             Path fileOutPath = new Path(args[1]);
             FileSystem fileSystem = FileSystem.get(configuration);
             if(fileSystem.exists(fileOutPath)){
-                fileSystem.delete(fileOutPath,true);
+                fileSystem.delete(fileOutPath, true);
             }
 
-            //int status = wordCountMR.run(args);
-            int status = ToolRunner.run(configuration,new MRTemplate(),args);
+            int status = ToolRunner.run(configuration, new MRTemplate(), args);
             System.exit(status);
-        } catch (Exception e) {
+
+        }catch (Exception e){
             e.printStackTrace();
         }
     }
