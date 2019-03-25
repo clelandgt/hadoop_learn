a0c1e2d4b9e2d9f5eb0ea248e768d58ffb7ad05f
cleland <clelandgt@163.com>
2019-03-25 19:13:00 +0800

    new word count mr

---
+++ hadoop_basics/src/main/java/club/cleland/hadoop_basics/mr/WordCount.java
@@ -0,0 +1,170 @@
+package club.cleland.hadoop_basics.mr;
+
+/**
+ * 求文本的单词数,测试文本:
+ *      hadoop,java,hadoop,spark,hbase,hbase
+ *      java,sqoop,flume,spark
+ *      python,flume,spark,hadoop,hadoop
+ * 结果:
+ *
+ *      flume	2
+ *      hadoop	4
+ *      hbase	2
+ *      java	2
+ *      python	1
+ *      spark	3
+ *      sqoop	1
+ */
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.IOException;
+
+
+public class WordCount extends Configured implements Tool {
+    /**
+     * Mapper
+     */
+    public static class WoldCountMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
+        private Text mapOutputKey = new Text();
+        private IntWritable mapOutputValue = new IntWritable(1);
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            //TODO
+
+        }
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            System.out.print("map keyIn: " + key + " ValueIN: " + value);
+            String rowString = value.toString();
+            String[] strs = rowString.split(",");
+            for(String str: strs){
+                mapOutputKey.set(str);
+                context.write(mapOutputKey, mapOutputValue);
+            }
+
+        }
+
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            //TODO
+        }
+    }
+
+    // Reduce
+    public static class WordCountReduce extends Reducer<Text, IntWritable, Text, IntWritable>{
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            //TODO
+        }
+
+        @Override
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for(IntWritable value: values){
+                sum += value.get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
+
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            //TODO
+        }
+    }
+
+
+    /**
+     *
+     * @param args
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public int run(String[] args) throws Exception {
+        // get config
+        Configuration configuration = new Configuration();
+
+        // create job
+        Job job = Job.getInstance(configuration, "WordCount");
+        job.setJarByClass(this.getClass());
+
+        // input
+        Path inputPath = new Path(args[0]);
+        FileInputFormat.addInputPath(job, inputPath);
+
+        // map
+        job.setMapperClass(WoldCountMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        // 1. partition分区
+        //job.setPartitionerClass();
+
+        // 2. 排序
+        //job.setSortComparatorClass();
+
+        // 3. combiner
+        //job.setCombinerClass();
+
+        // 4. compress
+        //configuration.set("mapreduce.map.output.compress","true");
+        //configuration.set("mapreduce.map.output.compress.codec","org.apache.hadoop.io.compress.SnappyCodec");
+
+        // 5. group分组
+        //job.setGroupingComparatorClass();
+
+        // reduce
+        job.setReducerClass(WordCountReduce.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        // output
+        Path outPath = new Path(args[1]);
+        FileOutputFormat.setOutputPath(job, outPath);
+
+        // commit
+        boolean isSuc = job.waitForCompletion(true);
+        return (isSuc) ? 0 : 1;
+    }
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args){
+        Configuration configuration = new Configuration();
+        args = new String[]{
+            "hdfs://loc-header:9000//data/input/wordcout_data.csv",
+            "hdfs://loc-header:9000//data/output/",
+        };
+        try{
+            Path fileOutPath = new Path(args[1]);
+            FileSystem fileSystem = FileSystem.get(configuration);
+            if(fileSystem.exists(fileOutPath)){
+                fileSystem.delete(fileOutPath, true);
+            }
+
+            int status = ToolRunner.run(configuration, new WordCount(), args);
+            System.exit(status);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
